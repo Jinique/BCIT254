ab02c9e712bff32b35b534b608ef28b13baf1c41
Jinique <44308010+Jinique@users.noreply.github.com>
2019-07-01 02:10:24 +1200

    Eyeball Maze v2.70 - LeRoi's View adoption starts

---
+++ src/main/java/com/example/eyeballmaze/GameActivityLeRoiV.java
@@ -0,0 +1,13 @@
+package com.example.eyeballmaze;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameActivityLeRoiV extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_le_roi_v);
+    }
+}
